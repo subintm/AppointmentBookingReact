02fd41cdaa53d45ca69d38ceb7ca6a71c91d2db1
subintm <subintmtm70@gmail.com>
2026-02-22 14:19:50 +0530

    routerUpdated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import AdminLogin from './Pages/Admin/AdminLogin'
 import AdminDashBoard from './Pages/Admin/AdminDashBoard'
 import Service from './Pages/Admin/Service'
@@ -10,15 +10,17 @@ import UserDashBoard from './Pages/UserPages/UserDashBoard'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AdminLogin />} />
-      <Route path="/register" element={<RegisterUser />} />
-      <Route path="/userdashboard" element={<UserDashBoard />} />
-      <Route path="/admindashboard" element={<AdminDashBoard />} />
-      <Route path='/servicePage' element={<Service/>} />
-      <Route path='/appointmentPage' element={<Appointments/>} />
-      <Route path='/availabilityPage' element={<Availability/>} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminLogin />} />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/userdashboard" element={<UserDashBoard />} />
+        <Route path="/admindashboard" element={<AdminDashBoard />} />
+        <Route path="/servicePage" element={<Service />} />
+        <Route path="/appointmentPage" element={<Appointments />} />
+        <Route path="/availabilityPage" element={<Availability />} />
+      </Routes>
+    </Router>
   )
 }
 
